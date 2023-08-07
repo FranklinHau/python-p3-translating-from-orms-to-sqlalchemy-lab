e674b514a82311795f186c7d50ee4877ccbc220a
Franklin Hau <fhauchi@gmail.com>
2023-08-06 18:20:40 -0600

    Translating-From-Orms-to-sqlAlquemy

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,41 @@
-from models import Dog
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Dog  # Ensure this import is correct
 
-def create_table(base):
-    pass
+# SQLite Database setup
+DATABASE_URL = 'sqlite:///dogs.db' 
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+
+def create_table(base, engine):
+    """Create the tables in the database using the provided declarative_base and engine."""
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    """Save a new dog or update an existing one."""
+    if not dog.name:
+        raise AttributeError("Dog instance must have a 'name' attribute.")
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    """Return all dogs."""
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    """Return dogs with a specific name."""
+    return session.query(Dog).filter(Dog.name == name).first()
 
 def find_by_id(session, id):
-    pass
+    """Return a dog with a specific ID."""
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    """Return dogs with a specific name and breed."""
+    return session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    """Update the breed of a dog."""
+    dog.breed = breed
+    session.add(dog)
+    session.commit()
